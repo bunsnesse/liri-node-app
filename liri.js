07f94b2eb1923683cd70853626345d969452550c
Emma <e.m.bunsness@gmail.com>
2019-08-22 23:20:57 -0600

    fixing bands in town

--- liri.js
+++ liri.js
@@ -1,115 +1,135 @@
 const dotenv = require("dotenv").config();
-var Spotify = require('node-spotify-api');
-const Twitter = require('twitter');
-const keys = require('./keys.js');
-var request = require('request');
-var fs = require('fs');
+
+// Load packages
+let NodeSpotify = require("node-spotify-api");
+let request = require("request");
+let moment = require("moment");
+let fs = require("fs");
+let keys = require("./keys.js");
+
+//store arguments
+let command = process.argv[2];
+let nodeArgs = process.argv;
 
 
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
-var omdbKey = keys.omdb.api_key;
+//string movie, song or artist
+let searchName = nodeArgs.slice(3).join("+");
+// console.log(searchName);
 
+//create function to search for movie
+function omdbMovie(movie) {
+    var omdbURL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
 
-const command = process.argv[2];
-const secondCommand = process.argv[3];
-
-switch (command) {
-    case ('my-tweets'):
-        getTweets();
-    break;
-    case ('spotify-this-song'):
-        if(secondCommand){
-            spotifyThisSong(secondCommand);
-         } else{
-            spotifyThisSong("My Heart Will Go On");
-         }
-    break;
-    case ('movie-this'):
-        if(secondCommand){
-            omdb(secondCommand);
-        } else{
-            omdb("Crazy, Stupid, Love.");
+    request(omdbURL, function (error, response, body) {
+        // console.log(response)
+        if (!error && response.statusCode == 200) {
+            var body = JSON.parse(body);
+            //console.log(body);
+            console.log(`----------------------------\nTitle: ${body.Title}\nRelease Year: ${body.Year}\nIMdB Rating: ${body.imdbRating}\nRotten Tomatoes Rating: ${body.tomatoRating}\nCountry: ${body.Country}\nLanguage: ${body.Language}\nPlot: ${body.Plot}\nActors: ${body.Actors}\n----------------------------`);
+            //added into log.txt
+            fs.appendFile("log.txt", `----------------------------\nTitle: ${body.Title}\nRelease Year: ${body.Year}\nIMdB Rating: ${body.imdbRating}\nRotten Tomatoes Rating: ${body.tomatoRating}\nCountry: ${body.Country}\nLanguage: ${body.Language}\nPlot: ${body.Plot}\nActors: ${body.Actors}\n----------------------------`, function (err) {
+                if (err) {
+                    console.log(err);
+                }
+            });
         }
-    break;
-    case ('do-what-it-says'):
-         doThing();
-    break;
-    default:
-        console.log('Try again');
-};
 
+        else {
+            console.log('Error occurred.')
+        }
+    })
+}
+//setting up command for movie-this
+if (command === "movie-this") {
+    if (searchName) {
+        omdbMovie(searchName);
+    }
+    if (searchName === "") {
+        console.log(`----------------------------\nIf you haven't watched Mr. Nobody, then you should: <http://www.imdb.com/title/tt0485947/>\nIt's on Netflix\n----------------------------`);
+        omdbMovie("Mr.Nobody");
+    }
+}
 
-function getTweets() {
-    client.get('statuses/home_timeline', function(error, tweets, response) {
-        if(error) throw error;
-       console.log("________________________________________");
-                    
-                    const tweets_parsed = tweets.map(word =>word.text);
-                    tweets_parsed.forEach(function(element) {
-                    console.log(element);
-                    });
+//Create function to search for band in town
+function bandInTown(artist) {
+    var bandInTownURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+    request(bandInTownURL, function (error, response, body) {
+        //console.log (response)
+        if (!error && response.statusCode == 200) {
+            var body = JSON.parse(body);
+            //console.log (body)
+            for (let i = 0; i < body.length; i++) {
+                console.log(`----------------------------\nName of Venue: ${body[i].venue.name}\nVenue Location: ${body[i].venue.city}, ${body[i].venue.region}, ${body[i].venue.country}\nDate of the Event: ${moment(body[i].datetime).format("dddd, MMMM Do YYYY, h:mm:ss a")}\n----------------------------`);
+
+                //add to log.txt
+                fs.appendFile("log.txt", `----------------------------\nName of Venue: ${body[i].venue.name}\nVenue Location: ${body[i].venue.city}, ${body[i].venue.region}, ${body[i].venue.country}\nDate of the Event: ${moment(body[i].datetime).format("dddd, MMMM Do YYYY, h:mm:ss a")}\n----------------------------`, function (err) {
+                    if (err) {
+                        console.log(err);
+                    }
                 });
             }
-function spotifyThisSong(song){
-    spotify.search({ type: 'track', query: song, limit: 1}, function(error, data){
-        if(!error){
-        for(var i = 0; i < data.tracks.items.length; i++){
-            var songData = data.tracks.items[i];
-                      //artist
-            console.log("Artist: " + songData.artists[0].name);
-                      //song name
-            console.log("Song: " + songData.name);
-                      //spotify preview link
-            console.log("Preview URL: " + songData.preview_url);
-                      //album name
-            console.log("Album: " + songData.album.name);
-            console.log("-----------------------");
-            } 
-        } else {
-        console.log('Error occurred.');
+        }
+    })
+}
+//setting up command for concert-this
+if (command === "concert-this") {
+    if (searchName) {
+        bandInTown(searchName);
+    }
+}
+
+//Create function to search for spotify song
+function searchSpotifySong(song) {
+    const spotify = new NodeSpotify(keys.spotify)
+
+    // console.log(spotify);
+
+    spotify.search({ type: 'track', query: song }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        else {
+            console.log(`----------------------------\nArtist: ${data.tracks.items[0].artists[0].name}\nThe song's name: ${data.tracks.items[0].name}\nPreview link of the song: ${data.tracks.items[0].preview_url}\nAlbum: ${data.tracks.items[0].album.name}\n----------------------------`);
+            
+            //Append these data into a log.txt file
+            fs.appendFile("log.txt", `----------------------------\nArtist: ${data.tracks.items[0].artists[0].name}\nThe song's name: ${data.tracks.items[0].name}\nPreview link of the song: ${data.tracks.items[0].preview_url}\nAlbum: ${data.tracks.items[0].album.name}\n----------------------------`, function (err) {
+                if (err) {
+                    console.log(err);
+                }
+            });
         }
     });
+}
+//setting up command for spotify-this-song
+if (command === "spotify-this-song") {
+    if (searchName) {
+        searchSpotifySong(searchName);
     }
+    if (searchName === "") {
+        searchSpotifySong("The Sign by Ace of Base")
+    }
+}
 
-    function omdb(movie){
-        var omdbURL = 'http://www.omdbapi.com/?t=' + movie + '&apikey=' + omdbKey + '&plot=short&tomatoes=true';
-      
-        request(omdbURL, function (error, response, body){
-          if(!error && response.statusCode == 200){
-            var body = JSON.parse(body);
-      
-            console.log("Title: " + body.Title);
-            console.log("Release Year: " + body.Year);
-            console.log("IMdB Rating: " + body.imdbRating);
-            console.log("Country: " + body.Country);
-            console.log("Language: " + body.Language);
-            console.log("Plot: " + body.Plot);
-            console.log("Actors: " + body.Actors);
-            console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
-            console.log("Rotten Tomatoes URL: " + body.tomatoURL);
-            
-          } else{
-            console.log('Error occurred.')
-          }
-          if(movie === "Mr. Nobody"){
-            console.log("-----------------------");
-            console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
-            console.log("It's on Netflix!");
 
-          }
-        });
-      
-      }
-      function doThing(){
-        fs.readFile('random.txt', "utf8", function(error, data){
-          var txt = data.split(',');
-      
-          spotifyThisSong(txt[1]);
-        });
-      }
-    
-        
+//create function for doWhatItSay 
+function doWhatitSay() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        }
+        //console.log(data);
+        var random = data.split(",")
+        // console.log(random);
+        // console.log(random[0]);
+        // console.log(random[1]);
+
+        searchSpotifySong(random[1])
+    });
+}
+if (command === "do-what-it-says") {
+    doWhatitSay();
+}
      
  
 
